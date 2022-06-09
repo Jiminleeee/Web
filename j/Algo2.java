ed50df28a895685edf37690ec5a04693ed072e05
Jiminleeee <jiminab94@naver.com>
2022-06-09 15:56:00 +0900

    수정

--- j/Algo2.java
+++ j/Algo2.java
@@ -9,12 +9,11 @@ public class Algo2 {
     int y = scanner.nextInt();  //공사 현장의 y좌표
     int R = scanner.nextInt();  //공사장 소음의 크기
     int count = scanner.nextInt();  //나무 그늘의 수 
-    int placeX; //그늘의 x좌표
-    int placeY; //그늘의 y좌표
+    
     //System.out.println(); //가독성 위해 한줄 띄우기(?)
-    for (int i = 1; i <= count; i++) { //나무 그늘의 수만큼
-      placeX = scanner.nextInt();   //그늘의 x좌표
-      placeY = scanner.nextInt();   //그늘의 y좌표
+    for (int i = 0; i < count; i++) { //나무 그늘의 수만큼
+      int placeX = scanner.nextInt();   //그늘의 x좌표
+      int placeY = scanner.nextInt();   //그늘의 y좌표
 
       //그늘의 위치가 공사현장에서 R미터 이상 떨어져 있을 때
       if (Math.pow((placeX - x), 2) + Math.pow((placeY - y), 2) >= Math.pow(R, 2)) {
@@ -25,9 +24,5 @@ public class Algo2 {
 
     }
 
-  }
-  //solution 활용해서 풀어보기ㅠㅠ
-  public static String solution() {
-    
   }
 }
