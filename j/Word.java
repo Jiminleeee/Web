909561a513f0acaa89fa7f9ce36af58251c98fe1
Jiminleeee <jiminab94@naver.com>
2022-06-09 13:08:20 +0900

    consonant

--- j/Word.java
+++ j/Word.java
@@ -4,7 +4,7 @@ public class Word {
 	public static void main(String[] args) {
 		Word word = new Word("Hello World");
 		System.out.println(word.isVowel(i(1)));
-		
+		System.out.println(word.isConsonant(0));
 		
 	}
 	private String letters;
@@ -15,11 +15,20 @@ public class Word {
 	
 	//i번째 글자가 모음인지 알려주는 메소드	//a,i,u,e,o
 	public boolean isVowel(int i) {
-		return letters.substring(i, i+1).equals("a") 
-				|| letters.substring(i, i+1).equals("i")
-				|| letters.substring(i, i+1).equals("u")
-				|| letters.substring(i, i+1).equals("e")
-				|| letters.substring(i, i+1).equals("o");
+		return letters.substring(i, i + 1).equals("a") 
+				|| letters.substring(i, i + 1).equals("i")
+				|| letters.substring(i, i + 1).equals("u")
+				|| letters.substring(i, i + 1).equals("e")
+				|| letters.substring(i, i + 1).equals("o");
+
+		// return "aiueo".contains(letters.substring(i, i + 1));
+
+	}
+
+	//i번째 글자가 자음인지  //a,i,u,e,o 제외한 나머지
+	public boolean isConsonant(int i) {
+		return "bcdfghjklmnpqrstvwxyz".contains(letters.substring(i, i + 1));
+		
 	}
 	
 }
